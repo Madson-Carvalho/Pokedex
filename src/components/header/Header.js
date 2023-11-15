d7c578381c71eadf34fea12bf0598314d1adee3c
RodrigoTDonde <rodrigotdonde@gmail.com>
2023-11-15 16:47:59 -0300

    adicionado o header no projeto Pokedex

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import img from "../../images/International_Pokémon_logo.svg.png"
+
+function Header() {
+    return (
+        <header>
+            <div>
+               <img src={img}/> 
+            </div>
+        </header>
+    )
+}
+
+export default Header;
